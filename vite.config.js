9770796f1de40620236f7f89f122f063bfa7ac50
Qadar Parvez <115987073+qadarparvez@users.noreply.github.com>
2025-07-17 13:26:46 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
       devSourcemap: false
     },
     server: {
-      host: 0.0.0.0,
+      host: true,
       port: 5173,
       strictPort: true,
       allowedHosts: [
